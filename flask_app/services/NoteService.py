e4e30eb3da84cf6af06024939d96f14978f488a6
gollanai <aidangollan@icloud.com>
2024-05-30 20:45:36 -0400

    add audio file

--- flask_app/services/NoteService.py
+++ flask_app/services/NoteService.py
@@ -5,6 +5,8 @@ class NoteService:
     @staticmethod
     def create_audio_note(courseId, userId, form, audio_file, keywords):
         try:
+
+
             note = SupabaseService.add_note(
                 courseId=courseId,
                 userId=userId,
@@ -15,8 +17,13 @@ class NoteService:
 
             if len(note) == 0:
                 return None
-            else:
-                return note[0]['id']
+            
+            fileId = SupabaseService.upload_file(audio_file, note[0]['id'], 'audio-files')
+
+            if fileId is None:
+                return None
+
+            return note[0]['id']
         except Exception as e:
             print("||||||||||EXCEPTION||||||||||")
             print(e)
--- flask_app/services/SupabaseService.py
+++ flask_app/services/SupabaseService.py
@@ -1,5 +1,11 @@
-from flask import current_app
+import tempfile
+import os
+
 from supabase import Client
+from werkzeug.datastructures import FileStorage
+
+from flask import current_app
+
 
 supabase: Client = current_app.config['SUPABASE_CLIENT']
 
@@ -12,7 +18,7 @@ class SupabaseService:
         form: str, 
         status: str = 'PENDING',
         content: str = ''
-    ):
+    ) -> list:
         return supabase.table('webapp-v2_note').insert({
             'courseId': courseId,
             'userId': userId,
@@ -20,3 +26,20 @@ class SupabaseService:
             'status': status,
             'content': content
         }).execute().data
+    
+    @staticmethod
+    def upload_file(
+        file: FileStorage, 
+        fileName: str, 
+        bucketName: str
+    ):
+        with tempfile.NamedTemporaryFile(delete=False) as tempFile:
+            file.save(tempFile.name)
+        
+        with open(tempFile.name, 'rb') as file_content:
+            response = supabase.storage.from_(bucketName).upload(fileName, file_content.read(), file_options={'content-type': 'audio/*'})
+        
+        os.remove(tempFile.name)
+
+        print(response.json())
+        return response.json()
