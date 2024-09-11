fc482aacc82a8db3b822346015c9d2223c57f85f
aidangollan <aidangollan@icloud.com>
2024-09-11 16:01:38 -0400

    notingtonian

--- flask_app/services/GraphQueryService.py
+++ flask_app/services/GraphQueryService.py
@@ -484,8 +484,10 @@ class GraphQueryService():
             WHERE $uuid IN root.uuid
             OPTIONAL MATCH (root)--(n1)
             WHERE n1:Document OR n1:Chunk OR n1:Concept
-            WITH root, n1
-            UNWIND [root, n1] AS n
+            OPTIONAL MATCH (n1)--(n2)
+            WHERE n2:Document OR n2:Chunk OR n2:Concept
+            WITH root, n1, n2
+            UNWIND [root, n1, n2] AS n
             WITH n WHERE n IS NOT NULL
             RETURN DISTINCT
                 CASE
@@ -493,13 +495,13 @@ class GraphQueryService():
                         id: ID(n),
                         labels: LABELS(n)[0],
                         fileName: n.fileName,
-                        noteId: [n.noteId]
+                        noteId: n.noteId
                     }}
                     WHEN n:Chunk THEN {{
                         id: ID(n),
                         labels: LABELS(n)[0],
                         offset: n.offset,
-                        noteId: [n.noteId]
+                        noteId: n.noteId
                     }}
                     WHEN n:Concept THEN {{
                         id: ID(n),
@@ -517,9 +519,11 @@ class GraphQueryService():
             WHERE $uuid IN root.uuid
             OPTIONAL MATCH (root)-[r1]-(n1)
             WHERE n1:Document OR n1:Chunk OR n1:Concept
-            UNWIND [r1] AS rel
+            OPTIONAL MATCH (n1)-[r2]-(n2)
+            WHERE n2:Document OR n2:Chunk OR n2:Concept
+            UNWIND [r1, r2] AS rel
             WITH rel WHERE rel IS NOT NULL
-            RETURN DISTINCT ID(startNode(rel)) AS start_node_id, ID(endNode(rel)) AS end_node_id, rel.type AS relationship_type, id(rel) AS relationship_id
+            RETURN DISTINCT ID(startNode(rel)) AS start_node_id, ID(endNode(rel)) AS end_node_id, type(rel) AS relationship_type
             """
 
             node_result = graphAccess.execute_query(node_query, {"uuid": uuid})
@@ -532,7 +536,7 @@ class GraphQueryService():
 
         except Exception as e:
             logging.error(f"Error executing query: {e}")
-            return None
+            return None, None
 
     @staticmethod
     def get_summary_for_param(param: str, id: str) -> str | None:
