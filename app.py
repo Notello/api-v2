bfef77bf75f129c019304c6767585679a9b9e663
Aidan Gollan <aidangollan@icloud.com>
2025-06-16 14:46:04 -0700

    python

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
 from flask_app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    # allow DO (or any host) to override the port
+    port = int(os.environ.get('PORT', 5000))
+    # bind to 0.0.0.0 so the container IP (10.x.x.x) is listening, not just localhost
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
